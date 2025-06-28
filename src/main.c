d6d3e9f3b34d8352909476d51b836009c612e246
Nikita Vetluzhskikh <trgrusted@gmail.com>
2025-06-28 20:34:34 +0200

    just test this code after 2 years

--- src/main.c
+++ src/main.c
@@ -17,13 +17,6 @@ int main()
     if ((ret_code = analyse())) {
         return ret_code;
     }
-    else {
-#ifdef PARS_DEBUG
-        fprintf(stderr, "\nERROR code %d\n", ret_code);
-#else
-        fprintf(stderr, "%d\n", ret_code);
-#endif
-    }
 
     return ER_NONE;
 }
