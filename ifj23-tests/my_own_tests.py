7d3a51a350adba83913f679cac7a514f5aa88a20
Nikita Vetluzhskikh <trgrusted@gmail.com>
2025-07-03 01:04:16 +0200

    test: add new

--- ifj23-tests/my_own_tests.py
+++ ifj23-tests/my_own_tests.py
@@ -2,7 +2,11 @@ from test_runtime import ErrorType, test, nr_failed
 
 
 def test_all():
+    # check rule
     test("let a = 5ssss","",expected_return_code=ErrorType.error_undefined_var)
+
+    # infinity recursion :_(
+    test("nil ?? 4","")
 
 if __name__ == "__main__":
     test_all()
