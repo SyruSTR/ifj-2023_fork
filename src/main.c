0434f9bdefcf58e3b301fb957d54730d9a70c701
Nikita Vetluzhskikh <trgrusted@gmail.com>
2025-07-02 13:23:14 +0200

    add debugging for 'attach to process...'

--- src/main.c
+++ src/main.c
@@ -7,12 +7,17 @@
  * @brief main brain
  */
 
+#include <signal.h>
+
 #include "parser.h"
 #include "error.h"
 #include <stdio.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+    if (argc > 2 && strcmp(argv[1],"-d") == 0) {
+        raise(SIGSTOP);
+    }
     int ret_code;
     if ((ret_code = analyse())) {
         return ret_code;
